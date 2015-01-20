19220d4429657022b9fd69e5a94fcc2f22fbdfdd
Christophe Limpalair <christf24@hotmail.com>
2015-01-20 18:01:50 -0500

    add logic

--- app/src/main/java/uscupstate/edu/myquiz/QuizActivity.java
+++ app/src/main/java/uscupstate/edu/myquiz/QuizActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -13,7 +14,41 @@ public class QuizActivity extends ActionBarActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
 
+    private TrueFalse[] mAnswerKey = new TrueFalse[]
+            {
+                    new TrueFalse(R.string.question_oceans, true),
+                    new TrueFalse(R.string.question_mideast, false),
+                    new TrueFalse(R.string.question_africa, false),
+                    new TrueFalse(R.string.question_americas, true),
+                    new TrueFalse(R.string.question_asia, true)
+            };
+
+    private int mCurrentIndex = 0;
+
+    private void updateQuestion()
+    {
+        int question = mAnswerKey[mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question);
+    }
+
+    public void checkAnswer(boolean userPressedTrue)
+    {
+        boolean answerIsTrue = mAnswerKey[mCurrentIndex].isTrueQuestion();
+
+        int messageResId = 0;
+
+        if (userPressedTrue == answerIsTrue)
+        {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
     /**
      * Method is called when an instance of the activity subclass is created.
      * This method is called by the OS.
@@ -27,21 +62,33 @@ public class QuizActivity extends ActionBarActivity {
 
         mTrueButton = (Button)findViewById(R.id.true_button);
         mFalseButton = (Button)findViewById(R.id.false_button);
+        mNextButton = (Button)findViewById(R.id.next_button);
 
         // listeners
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
 
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
+
+        mNextButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // % used to avoid array out of bounds error when you reach end of array
+                mCurrentIndex = (mCurrentIndex + 1) % mAnswerKey.length;
+                updateQuestion();
+            }
+        });
+
+        updateQuestion();
     }
 
 
@@ -66,4 +113,7 @@ public class QuizActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
---
+++ app/src/main/java/uscupstate/edu/myquiz/TrueFalse.java
@@ -0,0 +1,37 @@
+package uscupstate.edu.myquiz;
+
+/**
+ * Created by Christophe on 1/20/2015.
+ */
+public class TrueFalse {
+    // variable will hold a resource ID for a string
+    private int mQuestion;
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion)
+    {
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion()
+    {
+        return mQuestion;
+    }
+
+    public void setQuestion( int question )
+    {
+        mQuestion = question;
+    }
+
+    public boolean isTrueQuestion()
+    {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion( boolean trueQuestion)
+    {
+        mTrueQuestion = trueQuestion;
+    }
+}
+
