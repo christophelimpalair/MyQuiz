befaee3e3fbbf19cbde7ce38c9380c4b3afacabc
Christophe Limpalair <christf24@hotmail.com>
2015-01-28 14:55:03 -0500

    landscape mode support

--- app/src/main/java/uscupstate/edu/myquiz/QuizActivity.java
+++ app/src/main/java/uscupstate/edu/myquiz/QuizActivity.java
@@ -1,21 +1,27 @@
 package uscupstate.edu.myquiz;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class QuizActivity extends ActionBarActivity {
+public class QuizActivity extends Activity {
+
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
 
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mNextButton;
     private Button mPrevButton;
+    private ImageButton mNextButton;
     private TextView mQuestionTextView;
 
     private TrueFalse[] mAnswerKey = new TrueFalse[]
@@ -61,11 +67,14 @@ public class QuizActivity extends ActionBarActivity {
         // Call setContentView to inflate a layout and put it on screen.
         setContentView(R.layout.activity_quiz);
 
+        // d stands for Debug
+        Log.d(TAG, "onCreate() called");
+
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
 
         mTrueButton = (Button)findViewById(R.id.true_button);
         mFalseButton = (Button)findViewById(R.id.false_button);
-        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton = (ImageButton)findViewById(R.id.next_button);
 
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -108,9 +117,45 @@ public class QuizActivity extends ActionBarActivity {
             }
         });
 
+        if (savedInstanceState != null)
+        {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+
         updateQuestion();
     }
 
+    @Override
+    // This asks the compiler to ensure that the class actually has the method that
+    // you are attempting to override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -134,6 +179,12 @@ public class QuizActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
+    @Override
+    public void onSaveInstanceState( Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.d(TAG, "onSavedInstanceState");
+        // Bundle object that maps string keys to values
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
 
 }
